ff166425db872cf23fc6a9e4a3e69ec35fdd377a
Sohith Bandreddi <sohith73@gmail.com>
2024-02-18 16:52:23 +0530

    Updated nav bar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NaveBar'
+import { useSelector } from 'react-redux'
+import store from '../redux/store'
+import LoginNav from '../components/NavBar/LoginNav'
 
 const Home = () => {
+     const {signup} = useSelector((store) => store.auth)
      return (
           <>
-          <NavBar />
+          {signup ? (<LoginNav />):<NavBar />}
+          
           <div className='bg-[#fff3e3] w-full h-screen px-[40px]'>Home</div>
           </>
      )
