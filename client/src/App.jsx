7e0d4bdf9f6b01401c770f8e8b3518e3c1758581
Sohith Bandreddi <sohith73@gmail.com>
2024-02-18 11:37:58 +0530

    Routes done for starters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css'
+import React from 'react'
+import { BrowserRouter, Route,  Routes } from 'react-router-dom';
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Signin from "./pages/Signin"
+import SignUp  from "./pages/SIgnUp"
+
+
+
 
 function App() {
-
-
      return (
-          <>
-               <div className='bg-slate-500 w-full h-full'></div>
-          </>
+          // <div className='w-screen min-h-screen bg-black- flex flex-col'>
+               
+          // </div>
+          <BrowserRouter>
+          <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/about' element={<About />} />
+               <Route path='/dashboard' element={<Dashboard />} />
+               <Route path='/projects' element={<Projects />} />
+               <Route path='/signin' element={<Signin />} />
+               <Route path='/signup' element={<SignUp />} />
+          </Routes>
+          </BrowserRouter>
      )
 }
+
+
 
 export default App
